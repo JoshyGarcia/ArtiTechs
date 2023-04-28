fbd5f8e968d229c586c4631a22cbeffce31be937
Joshy Garcia <joshy.garcia@lc.cuny.edu>
2023-04-28 17:44:51 -0400

    Event Page

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -24,23 +24,26 @@ const Events = () => {
       <div className="flex flex-wrap justify-center ">
         <img src="/gamingTournament.png" className="w-1/3 aspect-auto h-1/3" />
         <p className=" w-1/3 self-center m-6">
-          Discover the exciting lineup of events happening in the vibrant city
-          of NYC! From music concerts and art exhibitions to sports matches and
-          cultural festivals, our Events section keeps you informed about the
-          latest happenings in the Big Apple. Explore the diverse and dynamic
-          event scene, and make sure you don't miss out on any of the thrilling
-          experiences that NYC has to offer.
+          Stay updated on the latest gaming events happening in New York City
+          (NYC) and dive into the vibrant gaming community. From thrilling
+          eSports tournaments to captivating game launches, this is where you'll
+          find all the exciting happenings in the gaming world. Explore a wide
+          range of events, including conventions, meetups, workshops, and much
+          more. Whether you're a casual gamer or a dedicated enthusiast, get
+          ready to immerse yourself in the thrilling world of gaming events in
+          NYC!
         </p>
       </div>
       <h2 className="text-2xl text-center mt-6 mb-6">Upcoming Events</h2>
       <p className="text-center w-1/2 self-center">
-        Stay ahead of the curve with our Upcoming Events section! We curate a
-        comprehensive list of upcoming events in NYC, keeping you in the loop
-        about what's coming next. Be the first to know about upcoming concerts,
-        theatrical performances, film screenings, conferences, and more. With
-        our constantly updated calendar, you can plan your schedule in advance
-        and ensure you never miss a remarkable event in the city that never
-        sleeps.
+        Get ready for an adrenaline-packed lineup of gaming events coming soon
+        to NYC!. Discover the hottest gaming conventions, thrilling game
+        showcases, exclusive previews, and exciting releases that are just
+        around the corner. Whether you're a fan of console gaming, PC gaming,
+        mobile gaming, or all of the above, this is the place to stay ahead of
+        the curve and make sure you don't miss out on any of the upcoming gaming
+        experiences in the Big Apple. Mark your calendars and get ready for
+        unforgettable gaming adventures!
       </p>
       <div className="flex flex-wrap justify-center">{eventsElements}</div>
     </>
